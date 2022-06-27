e59232ea2da5a1aafe1d1d986f7cc1bb9e4004f0
jerry <jerryms89@gmail.com>
2022-06-27 15:50:56 -0400

    first commit

---
+++ validacion.js
@@ -0,0 +1,10 @@
+function validarCorreo(correo){
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido = expReg.test(correo);
+    if(esValido==true){
+        alert("el correo electronico es valido");
+    }
+    else{
+        alert("el correo electronico no es valido");
+    }
+}
